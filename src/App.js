bb3da1c31602634f6c68c81cc84e18781f141712
Akshay <akshay.sharma76757@gmail.com>
2020-06-27 22:45:06 +0530

    useState hook every possible example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const initialState = 0;
+  const [counter, setCounter] = useState(initialState);
+  const [name, setName] = useState({
+    firstName: "",
+    lastName: "",
+  });
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: Math.floor(Math.random() * 10) + 1,
+      },
+    ]);
+  };
+
+  return (
+    <div className="App">
+      <div>{counter}</div>
+      <button onClick={() => setCounter((prevstate) => prevstate + 5)}>
+        Add
+      </button>
+      <button onClick={() => setCounter((prevstate) => prevstate - 5)}>
+        Substract
+      </button>
+      <button onClick={() => setCounter(initialState)}>Reset</button>
+
+      <div>First Name : {name.firstName}</div>
+      <div>Last Name : {name.lastName}</div>
+      <input
+        type="text"
+        value={name.firstName}
+        onChange={(e) => setName({ ...name, firstName: e.target.value })}
+      />
+      <input
+        type="text"
+        value={name.lastName}
+        onChange={(e) => setName({ ...name, lastName: e.target.value })}
+      />
+
+      <button onClick={addItem}>Click ME</button>
+      <ul>
+        {items.map((item) => (
+          <div key={item.id}>
+            <li>{item.value}</li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
