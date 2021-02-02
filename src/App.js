9dd0ce4c98cdbc71f323c0edfb579a829e0bca05
Akshay Sharma <akshay.sharma@BRIMACLAP028.local>
2021-02-02 20:21:07 +0530

    added useReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import Counter from "./component/Counter";
+import CounterTwo from "./component/CounterTwo";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <CounterTwo />
+    </div>
+  );
 }
 
 export default App;
